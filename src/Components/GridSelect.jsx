7779ed792f4a2b388ca800856ef48c14c58d8c80
SebBird <sebbird95@gmail.com>
2021-04-25 20:57:29 +0100

    add four grid

--- src/Components/GridSelect.jsx
+++ src/Components/GridSelect.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import styled from "styled-components";
 
-const GridSelect = ({gridSize, onGridChange}) => {
-    const GridBtn = styled.button`
-    border: none;
-    border-radius: 10px;
-    height: 40px;
-    width: 100px;
-    margin: 1rem;
-    background: gold;
-    box-shadow: 0 3px 0px 0px goldenrod;
-    transition: all 0.1s;
-    }
-    &:active {
-        transform: translateY(3px);
-        box-shadow: none;
-    }`;
+const Container = styled.div`
+display: flex`;
 
-    return ( 
-        <GridBtn onClick={onGridChange}>Grid: {gridSize} x {gridSize}</GridBtn>
+const GridButton = styled.button`
+border: none;
+border-radius: 10px;
+height: 40px;
+width: 100px;
+margin: 1rem 0;
+background: gold;
+box-shadow: 0 3px 0px 0px goldenrod;
+transition: all 0.1s;
+`;
+
+const ChangeBtn = styled.button`
+border: none;
+border-radius: 10px;
+height: 40px;
+width: 40px;
+margin: 1rem;
+background: gold;
+box-shadow: 0 3px 0px 0px goldenrod;
+font-size: 1.25rem;
+transition: all 0.1s;
+}
+&:active {
+    transform: translateY(3px);
+    box-shadow: none;
+`;
+
+const GridSelect = ({gridSize, onGridChange}) => {
+    return (
+        <Container>
+            <ChangeBtn onClick={() => onGridChange("-")}>-</ChangeBtn>
+            <GridButton>Grid: {gridSize} x {gridSize}</GridButton>
+            <ChangeBtn onClick={() => onGridChange("+")}>+</ChangeBtn>
+        </Container>
+
      );
 }
  
