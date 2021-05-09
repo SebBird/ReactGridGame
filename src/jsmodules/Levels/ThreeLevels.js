457497d8ac1b922a7133bdc6fe80e74b25e90410
Seb <sebbird95@gmail.com>
2021-05-09 12:22:35 +0100

    hardcode levels, export levels, begin match func

---
+++ src/jsmodules/Levels/ThreeLevels.js
@@ -0,0 +1,172 @@
+const threeLevels = [
+  [
+    {
+      id: 0,
+      highlight: true,
+      colour: "blue",
+      numbers: [1, 3],
+    },
+    {
+      id: 1,
+      highlight: false,
+      colour: "yellow",
+      numbers: [0, 2, 4],
+    },
+    {
+      id: 2,
+      highlight: true,
+      colour: "blue",
+      numbers: [1, 5],
+    },
+    {
+      id: 3,
+      highlight: false,
+      colour: "yellow",
+      numbers: [0, 4, 6],
+    },
+    {
+      id: 4,
+      highlight: false,
+      colour: "yellow",
+      numbers: [1, 3, 5, 7],
+    },
+    {
+      id: 5,
+      highlight: false,
+      colour: "yellow",
+      numbers: [2, 4, 8],
+    },
+    {
+      id: 6,
+      highlight: true,
+      colour: "yellow",
+      numbers: [3, 7],
+    },
+    {
+      id: 7,
+      highlight: false,
+      colour: "yellow",
+      numbers: [4, 6, 8],
+    },
+    {
+      id: 8,
+      highlight: true,
+      colour: "yellow",
+      numbers: [5, 7],
+    },
+  ],
+  [
+    {
+      id: 0,
+      highlight: true,
+      colour: "yellow",
+      numbers: [1, 3],
+    },
+    {
+      id: 1,
+      highlight: true,
+      colour: "yellow",
+      numbers: [0, 2, 4],
+    },
+    {
+      id: 2,
+      highlight: true,
+      colour: "blue",
+      numbers: [1, 5],
+    },
+    {
+      id: 3,
+      highlight: true,
+      colour: "yellow",
+      numbers: [0, 4, 6],
+    },
+    {
+      id: 4,
+      highlight: true,
+      colour: "blue",
+      numbers: [1, 3, 5, 7],
+    },
+    {
+      id: 5,
+      highlight: true,
+      colour: "yellow",
+      numbers: [2, 4, 8],
+    },
+    {
+      id: 6,
+      highlight: true,
+      colour: "blue",
+      numbers: [3, 7],
+    },
+    {
+      id: 7,
+      highlight: true,
+      colour: "yellow",
+      numbers: [4, 6, 8],
+    },
+    {
+      id: 8,
+      highlight: true,
+      colour: "yellow",
+      numbers: [5, 7],
+    },
+  ],
+  [
+    {
+      id: 0,
+      highlight: true,
+      colour: "blue",
+      numbers: [1, 3],
+    },
+    {
+      id: 1,
+      highlight: true,
+      colour: "blue",
+      numbers: [0, 2, 4],
+    },
+    {
+      id: 2,
+      highlight: true,
+      colour: "blue",
+      numbers: [1, 5],
+    },
+    {
+      id: 3,
+      highlight: false,
+      colour: "blue",
+      numbers: [0, 4, 6],
+    },
+    {
+      id: 4,
+      highlight: true,
+      colour: "yellow",
+      numbers: [1, 3, 5, 7],
+    },
+    {
+      id: 5,
+      highlight: false,
+      colour: "yellow",
+      numbers: [2, 4, 8],
+    },
+    {
+      id: 6,
+      highlight: true,
+      colour: "blue",
+      numbers: [3, 7],
+    },
+    {
+      id: 7,
+      highlight: true,
+      colour: "blue",
+      numbers: [4, 6, 8],
+    },
+    {
+      id: 8,
+      highlight: true,
+      colour: "blue",
+      numbers: [5, 7],
+    },
+  ],
+];
+
+export default threeLevels;
